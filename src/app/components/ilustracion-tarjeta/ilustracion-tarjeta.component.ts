9cb7ef39351945956107bbabb39b2fe7f1622d53
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-04-10 20:04:00 -0400

    #UP: * Se añade evento error al no poder cargar la imagen del cover. * Se modifica el evento de load al cargar la iamgen del cover.

--- src/app/components/ilustracion-tarjeta/ilustracion-tarjeta.component.ts
+++ src/app/components/ilustracion-tarjeta/ilustracion-tarjeta.component.ts
@@ -12,11 +12,37 @@ export class IlustracionTarjetaComponent implements OnInit {
   @Input() illustration: any = {};
   imageDefault = 'assets/img/system/screenWait.png';
   showDefault = true;
+  showWait = true;
+  showErrorMessage = false;
 
   constructor( private router: Router) {}
 
   ngOnInit(): void {}
 
+  /**
+   * Mostrar imagen al culminar su carga
+   */
+  showImage(){
+    setTimeout(() => {
+      this.showWait = !this.showWait;
+      this.showDefault = !this.showDefault;
+    }, 1500);
+  }
+
+  /**
+   * Mostrar mensage de Error al no cargar la imagen
+   */
+  showErrorImage(){
+    setTimeout(() => {
+      this.showWait = !this.showWait;
+      this.showErrorMessage = !this.showErrorMessage;
+      console.log('Error on load image');
+    }, 1500);
+  }
+
+  /**
+   * Mostrar ficha detallada de la Ilustracion
+   */
   showIllustrationDetails(): void{
     this.router.navigate(['/ilustracion', this.illustration.id]);
   }
