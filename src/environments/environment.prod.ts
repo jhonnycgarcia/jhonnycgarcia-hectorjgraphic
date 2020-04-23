9b28c21b11c8d6ca34656589c2d870c04063b1f1
Jhonny Garcia <jhonnycgarcia@gmail.com>
2020-04-23 01:15:51 -0400

    #UP: * Se añaden nuevos atributos a las variables de entorno. Tanto para desarrollo como para produccion.
    # OJO!
    * Estas nueva variables no seran actualizadas en el repositorio publico.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,13 @@ export const environment = {
   production: true,
   author: 'Héctor Carpio',
   instagram: 'https://www.instagram.com/heekops',
-  behance: 'https://www.behance.net/hectorjgraaf63'
+  behance: 'https://www.behance.net/hectorjgraaf63',
+  firebase: {
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,15 @@ export const environment = {
   production: false,
   author : 'Héctor Carpio',
   instagram: 'https://www.instagram.com/heekops',
-  behance: 'https://www.behance.net/hectorjgraaf63'
+  behance: 'https://www.behance.net/hectorjgraaf63',
+  firebase: {
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
 
 /*
